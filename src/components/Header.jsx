a3e68d5e670517bd0f9e5edec6195191e8b93bee
rafaelbitencourt <rafael_silbit@hotmail.com>
2020-08-06 20:01:18 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText
+} from '@material-ui/core';
+
 import MenuIcon from '@material-ui/icons/Menu';
+
 import AuthService from '../services/auth.service';
 import { ConfirmDialog } from '../components/Dialog';
 
@@ -23,10 +32,19 @@ export default ({ showMenu, showLoginRegister, showUserLogout, usuario }) => {
   const classes = useStyles();
   let history = useHistory();
   const [confirmOpen, setConfirmOpen] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const sair = () => {
     AuthService.logout();
     history.push("/");
+  };
+
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
   };
 
   return (
@@ -34,10 +52,10 @@ export default ({ showMenu, showLoginRegister, showUserLogout, usuario }) => {
       <AppBar position="static">
         <Toolbar>
           {showMenu &&
-            <IconButton hidden={showMenu} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <IconButton hidden={showMenu} onClick={() => handleDrawerOpen(true)} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
               <MenuIcon />
             </IconButton>}
-
+            
           <Typography variant="h6" className={classes.title}>
             EngeLive
           </Typography>
@@ -57,6 +75,26 @@ export default ({ showMenu, showLoginRegister, showUserLogout, usuario }) => {
           }
         </Toolbar>
       </AppBar>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={handleDrawerClose}
+      >
+        <List>
+          <ListItem button key='Projetos' onClick={() => {
+              history.push("/projetos");
+              setDrawerOpen(false);
+            }}>
+            <ListItemText primary='Projetos' />
+          </ListItem>
+          <ListItem button key='Materiais'>
+            <ListItemText primary='Materiais' />
+          </ListItem>
+          <ListItem button key='Colaboradores'>
+            <ListItemText primary='Colaboradores' />
+          </ListItem>
+        </List>
+      </Drawer>
       <ConfirmDialog
         titulo="Sair"
         mensagem="Tem certeza de que deseja sair?"
