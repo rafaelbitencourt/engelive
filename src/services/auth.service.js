dca961f5536b5d3418424deb7643b99e692bc97f
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-03-08 22:55:45 -0300

    Update auth.service.js

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-//const API_URL = "http://localhost:3001/auth/";
+ //const API_URL = "http://localhost:3001/auth/";
 const API_URL = "https://engeliveapi.herokuapp.com/auth/";
 
 class AuthService {
