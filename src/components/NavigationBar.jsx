8759ac8df5c7459db5644664b396d1cc47f6eb18
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-05-19 19:38:36 -0300

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -9,6 +9,9 @@ import {
     MenuItem,
     ListItemText,
     Collapse,
+    List,
+    ListItem,
+    Hidden
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -50,72 +53,93 @@ const NavigationBar = (props) => {
         return props.location.pathname === routeName ? true : false;
     }
 
-    return (
-        <div>
-            <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleDrawerOpen}>
-                <MenuIcon />
-            </IconButton>
-            <Drawer open={isOpen} onClose={handleDrawerClose}>
-                <MenuList>
-                    {/* {Routes.map((prop, key) => {
-                    return (
-                        <NavLink to={prop.path} style={{ textDecoration: 'none' }} key={key}>
-                        <MenuItem selected={activeRoute(prop.path)}>
-                            <ListItemText primary={prop.sidebarName} />
-                        </MenuItem>
-                        </NavLink>
-                    );
-                    })} */}
-                    <NavLink to={"/tipos_projetos"} style={{ textDecoration: 'none' }} key={"tipos_projetos"}>
-                        <MenuItem selected={activeRoute("/tipos_projetos")}>
-                            <ListItemText primary={"Tipos de projeto"} />
-                        </MenuItem>
+    const content = (<List>
+        {/* {Routes.map((prop, key) => {
+        return (
+            <NavLink to={prop.path} style={{ textDecoration: 'none' }} key={key}>
+            <MenuItem selected={activeRoute(prop.path)}>
+                <ListItemText primary={prop.sidebarName} />
+            </MenuItem>
+            </NavLink>
+        );
+        })} */}
+        <NavLink to={"/tipos_projetos"} style={{ textDecoration: 'none' }} key={"tipos_projetos"}>
+            <ListItem selected={activeRoute("/tipos_projetos")}>
+                <ListItemText /*primary={"Tipos de projeto"}*/ secondary={"Tipos de TESTE"} />
+            </ListItem>
+        </NavLink>
+        <NavLink to={"/obras"} style={{ textDecoration: 'none' }} key={"obras"}>
+            <ListItem selected={activeRoute("/obras")}>
+                <ListItemText primary={"Obras"} />
+            </ListItem>
+        </NavLink>
+        {obras.map((obra, key) => {
+            const path = `/obra/${obra.id}/projetos`;
+            return (
+                <div key={key}>
+                    <NavLink to={path} style={{ textDecoration: 'none' }} >
+                        <ListItem selected={activeRoute(path)}>
+                            <ListItemText primary={obra.descricao} />
+                        </ListItem>
                     </NavLink>
-                    <NavLink to={"/obras"} style={{ textDecoration: 'none' }} key={"obras"}>
-                        <MenuItem selected={activeRoute("/obras")}>
-                            <ListItemText primary={"Obras"} />
-                        </MenuItem>
-                    </NavLink>
-                    {obras.map((obra, key) => {
-                        const path = `/obra/${obra.id}/projetos`;
+                    {/* <Collapse in={isOpenProjetos[key]} timeout="auto" unmountOnExit> */}
+
+                    {/* </Collapse> */}
+                    {obra.projetos.map((projeto, key) => {
+                        const pathProjeto = `/obra/${obra.id}/projeto/${projeto.id}/plantas`;
                         return (
                             <div key={key}>
-                                <NavLink to={path} style={{ textDecoration: 'none' }} >
-                                    <MenuItem selected={activeRoute(path)}>
-                                        <ListItemText primary={obra.descricao} />
-                                    </MenuItem>
+                                <NavLink to={pathProjeto} style={{ textDecoration: 'none' }} >
+                                    <ListItem selected={activeRoute(pathProjeto)}>
+                                        <ListItemText primary={projeto.descricao} />
+                                    </ListItem>
                                 </NavLink>
-                                {/* <Collapse in={isOpenProjetos[key]} timeout="auto" unmountOnExit> */}
-
-                                {/* </Collapse> */}
-                                {obra.projetos.map((projeto, key) => {
-                                    const pathProjeto = `/obra/${obra.id}/projeto/${projeto.id}/plantas`;
+                                {projeto.plantas.map((planta, key) => {
+                                    const pathPlanta = `/obra/${obra.id}/projeto/${projeto.id}/planta/${planta.id}/detalhes`;
                                     return (
-                                        <div key={key}>
-                                            <NavLink to={pathProjeto} style={{ textDecoration: 'none' }} >
-                                                <MenuItem selected={activeRoute(pathProjeto)}>
-                                                    <ListItemText primary={projeto.descricao} />
-                                                </MenuItem>
-                                            </NavLink>
-                                            {projeto.plantas.map((planta, key) => {
-                                                const pathPlanta = `/obra/${obra.id}/projeto/${projeto.id}/planta/${planta.id}/detalhes`;
-                                                return (
-                                                    <NavLink to={pathPlanta} style={{ textDecoration: 'none' }} key={key}>
-                                                        <MenuItem selected={activeRoute(pathPlanta)}>
-                                                            <ListItemText primary={planta.descricao} />
-                                                        </MenuItem>
-                                                    </NavLink>
-                                                );
-                                            })}
-                                        </div>
+                                        <NavLink to={pathPlanta} style={{ textDecoration: 'none' }} key={key}>
+                                            <ListItem selected={activeRoute(pathPlanta)}>
+                                                <ListItemText primary={planta.descricao} />
+                                            </ListItem>
+                                        </NavLink>
                                     );
                                 })}
                             </div>
                         );
                     })}
-                </MenuList>
-            </Drawer>
-        </div>
+                </div>
+            );
+        })}
+    </List>
+    );
+
+    return (
+        <>
+            {/* <Hidden lgUp> */}
+                <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleDrawerOpen}>
+                    <MenuIcon />
+                </IconButton>
+                <Drawer open={isOpen} onClose={handleDrawerClose}>
+                    {content}
+                </Drawer>
+            {/* </Hidden>
+            <Hidden lgDown>
+                <Drawer
+                    anchor="left"
+                    open
+                    variant="persistent"
+                    PaperProps={{
+                        sx: {
+                            width: 256,
+                            top: 64,
+                            height: 'calc(100% - 64px)'
+                        }
+                    }}
+                >
+                    {content}
+                </Drawer>
+            </Hidden> */}
+        </>
     );
 };
 
