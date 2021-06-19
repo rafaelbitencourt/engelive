80523c47e32e3f888d8774bb7e27f21013beb664
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-06-19 11:35:34 -0300

    Update ListaCadastro.jsx

--- src/components/lista/ListaCadastro.jsx
+++ src/components/lista/ListaCadastro.jsx
@@ -4,9 +4,7 @@ import {
     IconButton,
     Box,
     Tooltip,
-    makeStyles,
     Typography,
-    Button,
     Paper
 } from '@material-ui/core';
 
@@ -16,17 +14,8 @@ import useAxios from 'axios-hooks';
 import Lista from 'components/lista/Lista'
 import { Loading, Error } from 'components'
 
-const useStyles = makeStyles({
-    add: {
-        position: "fixed",
-        bottom: 0,
-        right: 10
-    },
-});
-
 const ListaCadastro = ({ title, getMethod, deleteMethod, linkNew, getTextItem, getLinkItem, getLinkEdit }) => {
     const [{ data, loading, error }, refetch] = useAxios(getMethod, { useCache: false });
-    const classes = useStyles();
 
     const getContent = () => {
         if (loading) return <Loading />;
@@ -34,43 +23,14 @@ const ListaCadastro = ({ title, getMethod, deleteMethod, linkNew, getTextItem, g
         if (error) return <Error error={error} />
 
         if (!data?.length)
-            return <Box
-                height='100%'
-                width='100%'
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}
-                >
-                <Typography
-                    align="center"
-                    color="textPrimary"
-                    variant="h3"
-                >
-                    Não há registros
-                </Typography>
-                <Box p={1} />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    component={Link}
-                    to={linkNew}
-                >
-                    Novo
-                </Button>
-                <Box p={1} />
-            </Box>
+            return <Typography
+                align="center"
+                variant="h3"
+            >
+                Não há registros
+            </Typography>
 
         return <>
-            <Box className={classes.add}>
-                <Tooltip title="Novo" >
-                    <IconButton variant="contained" color="primary" component={Link} to={linkNew}>
-                        <AddCircle fontSize="large" />
-                    </IconButton>
-                </Tooltip>
-            </Box>
             <Lista
                 rows={data}
                 deleteMethod={deleteMethod}
@@ -87,16 +47,31 @@ const ListaCadastro = ({ title, getMethod, deleteMethod, linkNew, getTextItem, g
             display="flex"
             flexDirection="column"
             height="100%"
-            pt={2}
-            pb={2}
+            pb={1}
         >
-            <Box pl={2}>
+            <Box
+                pl={2}
+                pt={1}
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+            >
                 <Typography
-                    color="textPrimary"
                     variant="h3"
                 >
                     {title}
                 </Typography>
+                <Tooltip
+                    title="Novo"
+                >
+                    <IconButton
+                        variant="contained"
+                        component={Link}
+                        to={linkNew}
+                    >
+                        <AddCircle />
+                    </IconButton>
+                </Tooltip>
             </Box>
             {getContent()}
         </Box>
