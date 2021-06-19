5a0921800b56bf142008b97d67f02b7e6562ba39
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-06-19 15:47:40 -0300

    Componente DetalhesPlanta

--- src/components/plantas/index.js
+++ src/components/plantas/index.js
@@ -1,2 +1,3 @@
 export { default as ListaPlantas } from './ListaPlantas';
 export { default as CadastroPlanta } from './CadastroPlanta';
+export { default as DetalhesPlanta } from './DetalhesPlanta';
